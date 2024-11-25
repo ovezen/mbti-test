3f0d0ddfeb50ed33d28a190a5c291595851884eb
ovezen <klimen1005@gmail.com>
2024-11-26 05:04:50 +0900

    fix: Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,17 +3,25 @@ import AuthForm from "../components/AuthForm";
 
 const Signup = () => {
   return (
-    <div>
-      <div>
-        <h1>회원가입</h1>
-        <AuthForm mode="signup" />
-        <div>
-          <p>
-            이미 계정이 있으신가요? <Link to="/login">로그인</Link>
-          </p>
+    <>
+      {/* header 들어갈 곳 */}
+      <div className="flex flex-col items-center justify-center bg-gray-100">
+        <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-lg">
+          <h1 className="text-4xl font-bold text-primary-color mb-6 text-center">
+            회원가입
+          </h1>
+          <AuthForm mode="signup" />
+          <div className="mt-4 text-center">
+            <p className="text-gray-600">
+              이미 계정이 있으신가요?{" "}
+              <Link to="/login" className="text-primary-color hover:underline">
+                로그인
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
