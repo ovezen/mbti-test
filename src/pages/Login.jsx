aedeebec34e84c0b478977c2bd43cc1af4d29894
ovezen <klimen1005@gmail.com>
2024-11-26 05:00:48 +0900

    fix: Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,17 +5,20 @@ const Login = () => {
   return (
     <>
       {/* header 들어갈 곳 */}
-    <div>
-      <div>
-        <h1>로그인</h1>
-        <AuthForm mode="login" />
-        <div>
-          <p>
-            계정이 없으신가요? <Link to="/signup">회원가입</Link>
-          </p>
+      <div className="w-full flex flex-col items-center justify-center bg-gray-100">
+        <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
+          <h1 className="text-3xl font-bold text-primary-color mb-6">로그인</h1>
+          <AuthForm mode="login" />
+          <div className="mt-4">
+            <p className="text-gray-600">
+              계정이 없으신가요?{" "}
+              <Link to="/signup" className="text-primary-color hover:underline">
+                회원가입
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
